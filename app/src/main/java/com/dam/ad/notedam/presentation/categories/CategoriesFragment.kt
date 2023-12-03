a768d146a033c867fcaba9bb85bc4723cd0ef413
kuromiichi <afbarranco@gmail.com>
2023-12-03 21:36:06 +0100

    Modificada la UI de la barra de navegación y mensaje de primer uso

--- app/src/main/java/com/dam/ad/notedam/presentation/categories/CategoriesFragment.kt
+++ app/src/main/java/com/dam/ad/notedam/presentation/categories/CategoriesFragment.kt
@@ -11,7 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class CategoriesFragment : Fragment() {
 
-    private var _binding:FragmentCategoriesBinding? = null
+    private var _binding: FragmentCategoriesBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
