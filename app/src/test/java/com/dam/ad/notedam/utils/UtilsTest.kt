b4801c2a424a84a398cd15dfdbe33700df0695c9
SergioDeIscarValera <seroigres888@gmail.com>
2023-12-04 22:03:44 +0100

    NoteDam JLP v0.09
    Test de los servicios Ok.

--- app/src/test/java/com/dam/ad/notedam/utils/UtilsTest.kt
+++ app/src/test/java/com/dam/ad/notedam/utils/UtilsTest.kt
@@ -2,6 +2,7 @@ package com.dam.ad.notedam.utils
 
 import android.net.Uri
 import com.dam.ad.notedam.models.Category
+import com.dam.ad.notedam.models.Note
 import com.dam.ad.notedam.models.SublistItem
 import java.io.File
 import java.time.LocalDateTime
@@ -9,17 +10,16 @@ import java.util.UUID
 
 class UtilsTest {
     companion object{
-        val id = UUID.randomUUID()
-      val categories = listOf( Category(id, "Paisajes", "Sitios que visitar", 0u, listOf(
-            com.dam.ad.notedam.models.Note.Text(UUID.randomUUID(), "Esto es una tarea de texto", false, LocalDateTime.now()),
-            com.dam.ad.notedam.models.Note.Image(UUID.randomUUID(), "https://img.freepik.com/fotos-premium/hermosos-paisajes-paisajes-naturales-hacen-que-personas-relajen-disfruten-fondo-pantalla_917506-214429.jpg", false, LocalDateTime.now()),
-            com.dam.ad.notedam.models.Note.Audio(UUID.randomUUID(), File("url de la nota de audio"), false, LocalDateTime.now()),
-            com.dam.ad.notedam.models.Note.Sublist(UUID.randomUUID(), listOf(SublistItem(false, "Tarea 1"), SublistItem(false, "Tarea 2")), true, LocalDateTime.now()),
-            com.dam.ad.notedam.models.Note.Text(UUID.randomUUID(), "Esto es otra tarea de texto", false, LocalDateTime.now()),
-            com.dam.ad.notedam.models.Note.Image(UUID.randomUUID(), "https://www.hofmann.es/blog/wp-content/uploads/2021/09/HF_3_Paisajes_WEB-23.jpg", false, LocalDateTime.now()),
-            com.dam.ad.notedam.models.Note.Sublist(UUID.randomUUID(), listOf(), false, LocalDateTime.now()),
-            com.dam.ad.notedam.models.Note.Text(UUID.randomUUID(), "Esto es la última tarea de texto", false, LocalDateTime.now()),
-            com.dam.ad.notedam.models.Note.Image(UUID.randomUUID(), "https://images.hola.com/imagenes/viajes/20200401164667/paisajes-de-espana-a-vista-de-pajaro-drone-aereas/0-806-353/paisajes-a-vista-de-pajaro-t.jpg", false, LocalDateTime.now())))
-    )
+      val categories = listOf( Category(UUID.randomUUID(), "Paisajes", "Sitios que visitar", 0u, mutableMapOf(
+            UUID.fromString("9e3a85dc-92e7-11ee-b9d1-0242ac120002") to Note.Text(UUID.fromString("9e3a85dc-92e7-11ee-b9d1-0242ac120002"), "Esto es una tarea de texto", false, LocalDateTime.now()),
+            UUID.fromString("f7ea82f8-92e7-11ee-b9d1-0242ac120002") to Note.Image(UUID.fromString("f7ea82f8-92e7-11ee-b9d1-0242ac120002") , "https://img.freepik.com/fotos-premium/hermosos-paisajes-paisajes-naturales-hacen-que-personas-relajen-disfruten-fondo-pantalla_917506-214429.jpg", false, LocalDateTime.now()),
+            UUID.fromString("fe4ee224-92e7-11ee-b9d1-0242ac120003") to Note.Audio(UUID.fromString("fe4ee224-92e7-11ee-b9d1-0242ac120003"), File("url de la nota de audio"), false, LocalDateTime.now()),
+            UUID.fromString("9e3a85dc-92e7-11ee-b9d1-0242ac120005") to Note.Sublist(UUID.fromString("9e3a85dc-92e7-11ee-b9d1-0242ac120005"), mutableMapOf("Tarea 1" to SublistItem(false, "Tarea 1"), "Tarea 2" to SublistItem(false, "Tarea 2")), true, LocalDateTime.now()),
+            UUID.fromString("9e3a85dc-92e7-11ee-b9d1-0242ac120006") to Note.Text(UUID.fromString("9e3a85dc-92e7-11ee-b9d1-0242ac120006"), "Esto es otra tarea de texto", false, LocalDateTime.now()),
+            UUID.fromString("9e3a85dc-92e7-11ee-b9d1-0242ac120007") to Note.Image(UUID.fromString("9e3a85dc-92e7-11ee-b9d1-0242ac120007"), "https://www.hofmann.es/blog/wp-content/uploads/2021/09/HF_3_Paisajes_WEB-23.jpg", false, LocalDateTime.now()),
+            UUID.fromString("9e3a85dc-92e7-11ee-b9d1-0242ac120008") to Note.Sublist(UUID.fromString("9e3a85dc-92e7-11ee-b9d1-0242ac120008"), mutableMapOf(), false, LocalDateTime.now()),
+            UUID.fromString("9e3a85dc-92e7-11ee-b9d1-0242ac120009") to Note.Text(UUID.fromString("9e3a85dc-92e7-11ee-b9d1-0242ac120009"), "Esto es la última tarea de texto", false, LocalDateTime.now()),
+            UUID.fromString("9e3a85dc-92e7-11ee-b9d1-0242ac120010") to Note.Image(UUID.fromString("9e3a85dc-92e7-11ee-b9d1-0242ac120010"), "https://images.hola.com/imagenes/viajes/20200401164667/paisajes-de-espana-a-vista-de-pajaro-drone-aereas/0-806-353/paisajes-a-vista-de-pajaro-t.jpg", false, LocalDateTime.now()))),
+      )
     }
 }
