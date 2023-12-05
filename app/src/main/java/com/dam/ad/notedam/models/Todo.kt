24fd445d917a695ace4d4112ef1b117c96b9ab02
kuromiichi <afbarranco@gmail.com>
2023-12-05 14:33:02 +0100

    Añadida propiedad type a las clases de Todo y eliminada la clase Sublist

--- app/src/main/java/com/dam/ad/notedam/models/Todo.kt
+++ app/src/main/java/com/dam/ad/notedam/models/Todo.kt
@@ -5,41 +5,46 @@ import java.time.LocalDate
 sealed class Todo {
     abstract val title: String
     abstract val createdAt: LocalDate
+    abstract val type: TodoType
     abstract var completed: Boolean
+
+    enum class TodoType { TEXT, IMAGE, AUDIO, SUBLIST }
 
     data class TextTodo(
         override val title: String,
         override val createdAt: LocalDate,
         override var completed: Boolean,
         val text: String
-    ) : Todo()
+    ) : Todo() {
+        override val type = TodoType.TEXT
+    }
 
     data class ImageTodo(
         override val title: String,
         override val createdAt: LocalDate,
         override var completed: Boolean,
         val image: String
-    ) : Todo()
+    ) : Todo() {
+        override val type = TodoType.IMAGE
+    }
 
     data class AudioTodo(
         override val title: String,
         override val createdAt: LocalDate,
         override var completed: Boolean,
         val audio: String
-    ) : Todo()
+    ) : Todo() {
+        override val type = TodoType.AUDIO
+    }
 
     data class SublistTodo(
         override val title: String,
         override val createdAt: LocalDate,
         override var completed: Boolean,
-        val sublist: Sublist
+        val sublist: MutableList<SublistItem>
     ) : Todo() {
-        val items = sublist.items
+        override val type = TodoType.SUBLIST
     }
-}
-
-data class Sublist(val items: MutableList<SublistItem>) {
-    fun add(title: String, completed: Boolean = false) = items.add(SublistItem(title, completed))
 }
 
 data class SublistItem(
