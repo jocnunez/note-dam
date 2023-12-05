42039bb589621f845c027f862f7422c7ad7cff9a
kuromiichi <afbarranco@gmail.com>
2023-12-05 09:46:03 +0100

    Modelos modificados (campo createdAt y rework de Todo a sealed class con herencia)

--- app/src/main/java/com/dam/ad/notedam/models/Category.kt
+++ app/src/main/java/com/dam/ad/notedam/models/Category.kt
@@ -1,6 +1,9 @@
 package com.dam.ad.notedam.models
 
+import java.time.LocalDate
+
 data class Category(
     val name: String,
+    val createdAt: LocalDate,
     val todos: MutableList<Todo>
 )
--- app/src/main/java/com/dam/ad/notedam/models/Todo.kt
+++ app/src/main/java/com/dam/ad/notedam/models/Todo.kt
@@ -1,6 +1,48 @@
 package com.dam.ad.notedam.models
 
-data class Todo(
+import java.time.LocalDate
+
+sealed class Todo {
+    abstract val title: String
+    abstract val createdAt: LocalDate
+    abstract var completed: Boolean
+
+    data class TextTodo(
+        override val title: String,
+        override val createdAt: LocalDate,
+        override var completed: Boolean,
+        val text: String
+    ) : Todo()
+
+    data class ImageTodo(
+        override val title: String,
+        override val createdAt: LocalDate,
+        override var completed: Boolean,
+        val image: String
+    ) : Todo()
+
+    data class AudioTodo(
+        override val title: String,
+        override val createdAt: LocalDate,
+        override var completed: Boolean,
+        val audio: String
+    ) : Todo()
+
+    data class SublistTodo(
+        override val title: String,
+        override val createdAt: LocalDate,
+        override var completed: Boolean,
+        val sublist: Sublist
+    ) : Todo() {
+        val items = sublist.items
+    }
+}
+
+data class Sublist(val items: MutableList<SublistItem>) {
+    fun add(title: String, completed: Boolean = false) = items.add(SublistItem(title, completed))
+}
+
+data class SublistItem(
     val title: String,
-    val content: String
+    val completed: Boolean = false
 )
