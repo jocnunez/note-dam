9dc1601f4bfab32f8098ca68e5192afdb9f25984
kuromiichi <afbarranco@gmail.com>
2023-12-07 20:22:11 +0100

    Añadido campo UUID en Category

--- app/src/main/java/com/dam/ad/notedam/models/Category.kt
+++ app/src/main/java/com/dam/ad/notedam/models/Category.kt
@@ -1,8 +1,10 @@
 package com.dam.ad.notedam.models
 
 import java.time.LocalDate
+import java.util.UUID
 
 data class Category(
+    val uuid: UUID,
     val name: String,
     val createdAt: LocalDate,
     val todos: MutableList<Todo>
